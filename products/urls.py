d9d7e2f9be66019ce550af9eaeafc5c2b01ec891
levin-mutai <levinmutai2@gmail.com>
2023-09-17 16:55:18 +0300

    added customer routes

--- products/urls.py
+++ products/urls.py
@@ -1,11 +1,15 @@
 from rest_framework.routers import DefaultRouter
-from .views import ProductViews,OrderViewSet,OrderProductViewSet
+from .views import ProductViews, OrderViewSet, OrderProductViewSet, CustomerViews
 
 
 router = DefaultRouter(trailing_slash=False)
 
 router.register(r"products", ProductViews, basename="Products_detail")
 router.register(r"orders", OrderViewSet, basename="Orders_detail")
-router.register(r"ordered_products", OrderProductViewSet, basename="Ordered_Products_detail")
+router.register(
+    r"ordered_products", OrderProductViewSet, basename="Ordered_Products_detail"
+)
+router.register("customers", CustomerViews, basename="customer_detail")
+
 
 urlpatterns = router.urls
